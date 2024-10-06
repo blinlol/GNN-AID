8bfd984fe80b94b458250bacdcec4326158f983c
Balabanov Fedor <f.balabanov@ispras.ru>
2024-10-06 06:48:24 +0000

    raw dynamic

--- src/nas/test.py
+++ src/nas/test.py
@@ -150,15 +150,23 @@ cfg_dir = "/home/ubuntu/GNN-AID/src/nas/cfg/"
 #                1, False)
 # random_experiments(cfg_dir + "34.yml")
 
-cfgs = [
-    cfg_dir + str(i) + '.yml' for i in range(1, 17)
-]
+# cfgs = [
+#     cfg_dir + str(i) + '.yml' for i in range(1, 17)
+# ]
 
-for cfg_file in cfgs:
-    try:
-        experiment(cfg_file)
-    except Exception as e:
-        logger.error("cfg_file = %r\n%s", cfg_file, e)
+# for cfg_file in cfgs:
+#     try:
+#         experiment(cfg_file)
+#     except Exception as e:
+#         logger.error("cfg_file = %r\n%s", cfg_file, e)
 
 # to_yaml_file("cfg/all.yml", ExperimentArgs(dataset=Datasets.bzr))
 # experiment(cfg_dir + "all.yml")
+
+
+
+ss = SearchSpace(Datasets.cora.value, SearchSpaceArgs(debug=False))
+nas = NasController(ss, ControllerArgs(dynamic_behaviour=DynamicBehaviourType.raw))
+trainer = Trainer(ss, nas, TrainerArgs())
+
+trainer.train()
